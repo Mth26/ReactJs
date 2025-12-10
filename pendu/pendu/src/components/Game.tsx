e3b75a53f8d75f964736a0dc188cc81fc4d338f2
Mathieu <214662154+Mth26@users.noreply.github.com>
2025-12-10 12:57:35 +0100

    ajout des états useState dans Game.tsx

--- pendu/pendu/src/components/Game.tsx
+++ pendu/pendu/src/components/Game.tsx
@@ -8,7 +8,7 @@ import frenchWords from "an-array-of-french-words";
 
 
 const getRandomWord = (): string => {
-    // 1. On filtre pour avoir que des mots entre 5 et 10 lettres (pour la difficulté)
+    // 1. filtre pour avoir que des mots entre 5 et 10 lettres (pour la difficulté)
     const filteredWords = frenchWords.filter(word => word.length >= 5 && word.length <= 10);
 
     const randomIndex = Math.floor(Math.random() * filteredWords.length);
@@ -16,7 +16,29 @@ const getRandomWord = (): string => {
     return filteredWords[randomIndex].toUpperCase();
 };
 
+const createLetterStates = (word: string): LetterState[] => {
+    const characters = word.split('');
+
+    const letterStates = characters.map((char) => {
+        return {
+            display: char,
+            state: 'Hidden' as const  
+        };
+    });
+
+    return letterStates;
+};
+
 export default function Game() {
+
+    const [wordLetters, setWordLetters] = useState<LetterState[]>(
+        createLetterStates(getRandomWord())
+    );
+    
+    const [playedLetters, setPlayedLetters] = useState<string[]>([]);
+    
+    const [errors, setErrors] = useState<number>(0);
+    const maxErrors = 6;
 
     const resetGame = () => {
         console.log("Nouvelle partie lancée !");
